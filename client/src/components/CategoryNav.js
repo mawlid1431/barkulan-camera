06bb72411a236503ab9140421e50e826463792cc
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-07-31 15:51:48 +0300

    caterogy is done

--- client/src/components/CategoryNav.js
+++ client/src/components/CategoryNav.js
@@ -1,7 +1,49 @@
 import React from 'react';
 
+//usefecht hooks
+import useFetch from '../hooks/useFetch';
+// links will be here 
+import { Link } from 'react-router-dom';
+
+
 const CategoryNav = () => {
-  return <div>CategoryNav</div>;
+  const { data } = useFetch('/categories');
+  // console.log(data)
+
+  return <aside className='hidden xl:flex' >
+
+
+    <div className='bg-primary flex flex-col  w-[286px] h-[500px] rounded-[8px] overflow-hidden' >
+      <div className='bg-accent py-4  text-primary uppercase  font-semibold flex-item-center justify-center'  >Browse Categories</div>
+
+      <div className='flex flex-col gap-y-6 p-6'>
+        {data?.map(Category => {
+          console.log(Category);
+
+          return <Link>
+
+            Category
+
+
+          </Link>
+
+
+        })}
+
+
+
+
+
+
+      </div>
+
+
+
+    </div>
+
+
+
+  </aside>;
 };
 
 export default CategoryNav;
